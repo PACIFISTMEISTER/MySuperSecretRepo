bd42b9308a4d2e34153443c01d06438c64c88b3a
Raging <2mfree@mail.ru>
2022-09-27 16:54:03 +0500

    refactoring

--- static/js/holder.js
+++ static/js/holder.js
@@ -1,12 +1,4 @@
-/*
 
-Holder - 2.2 - client side image placeholders
-(c) 2012-2013 Ivan Malopinsky / http://imsky.co
-
-Provided under the MIT License.
-Commercial use requires attribution.
-
-*/
 
 var Holder = Holder || {};
 (function (app, win) {
@@ -22,7 +14,7 @@ if (!canvas.getContext) {
 } else {
 	if (canvas.toDataURL("image/png")
 		.indexOf("data:image/png") < 0) {
-		//Android doesn't support data URI
+
 		fallback = true;
 	} else {
 		var ctx = canvas.getContext("2d");
@@ -129,22 +121,21 @@ app.flags = {
 	}
 }
 
-//getElementsByClassName polyfill
 document.getElementsByClassName||(document.getElementsByClassName=function(e){var t=document,n,r,i,s=[];if(t.querySelectorAll)return t.querySelectorAll("."+e);if(t.evaluate){r=".//*[contains(concat(' ', @class, ' '), ' "+e+" ')]",n=t.evaluate(r,t,null,0,null);while(i=n.iterateNext())s.push(i)}else{n=t.getElementsByTagName("*"),r=new RegExp("(^|\\s)"+e+"(\\s|$)");for(i=0;i<n.length;i++)r.test(n[i].className)&&s.push(n[i])}return s})
 
-//getComputedStyle polyfill
+
 window.getComputedStyle||(window.getComputedStyle=function(e){return this.el=e,this.getPropertyValue=function(t){var n=/(\-([a-z]){1})/g;return t=="float"&&(t="styleFloat"),n.test(t)&&(t=t.replace(n,function(){return arguments[2].toUpperCase()})),e.currentStyle[t]?e.currentStyle[t]:null},this})
 
-//http://javascript.nwbox.com/ContentLoaded by Diego Perini with modifications
+
 function contentLoaded(n,t){var l="complete",s="readystatechange",u=!1,h=u,c=!0,i=n.document,a=i.documentElement,e=i.addEventListener?"addEventListener":"attachEvent",v=i.addEventListener?"removeEventListener":"detachEvent",f=i.addEventListener?"":"on",r=function(e){(e.type!=s||i.readyState==l)&&((e.type=="load"?n:i)[v](f+e.type,r,u),!h&&(h=!0)&&t.call(n,null))},o=function(){try{a.doScroll("left")}catch(n){setTimeout(o,50);return}r("poll")};if(i.readyState==l)t.call(n,"lazy");else{if(i.createEventObject&&a.doScroll){try{c=!n.frameElement}catch(y){}c&&o()}i[e](f+"DOMContentLoaded",r,u),i[e](f+s,r,u),n[e](f+"load",r,u)}}
 
-//https://gist.github.com/991057 by Jed Schmidt with modifications
+
 function selector(a){
 	a=a.match(/^(\W)?(.*)/);var b=document["getElement"+(a[1]?a[1]=="#"?"ById":"sByClassName":"sByTagName")](a[2]);
 	var ret=[];	b!==null&&(b.length?ret=b:b.length===0?ret=b:ret=[b]);	return ret;
 }
 
-//shallow object property extend
+
 function extend(a,b){
 	var c={};
 	for(var i in a){
@@ -160,14 +151,14 @@ function extend(a,b){
 	return c
 }
 
-//hasOwnProperty polyfill
+
 if (!Object.prototype.hasOwnProperty)
-    /*jshint -W001, -W103 */
+
     Object.prototype.hasOwnProperty = function(prop) {
 		var proto = this.__proto__ || this.constructor.prototype;
 		return (prop in this) && (!(prop in proto) || proto[prop] !== this[prop]);
 	}
-    /*jshint +W001, +W103 */
+
 
 function text_size(width, height, template) {
 	height = parseInt(height, 10);
@@ -216,7 +207,6 @@ function draw(args) {
 	if (text_width / width >= 0.75) {
 		text_height = Math.floor(text_height * 0.75 * (width / text_width));
 	}
-	//Resetting font size if necessary
 	ctx.font = "bold " + (text_height * ratio) + "px " + font;
 	ctx.fillText(text, (width / 2), (height / 2), width);
 	return canvas.toDataURL("image/png");
@@ -364,7 +354,7 @@ function parse_flags(flags, options) {
 		} else if (app.flags.colors.match(flag)) {
 			ret.theme = app.flags.colors.output(flag);
 		} else if (options.themes[flag]) {
-			//If a theme is specified, it will override custom colors
+
 			if(options.themes.hasOwnProperty(flag)){
 				ret.theme = extend(options.themes[flag], {});
 			}
